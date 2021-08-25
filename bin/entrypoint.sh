c564d25aa02f39cbffbf6958bbf2b38a16bc8d53
Dario B <feddar@users.noreply.github.com>
2021-08-25 17:10:22 +0200

    Create entrypoint.sh

---
+++ bin/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+maldet -u
+maldet -d
+
+echo "to scan a directory run (for example):"
+echo "maldet -a /var/www/clients"
+echo "to show the last report:"
+echo "maldet --report"
+echo "to show the last log:"
+echo "maldet -l"
+echo "to show the report list:"
+echo "maldet --report list"
+
+exec "$@"
